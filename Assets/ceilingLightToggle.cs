bb08eba0f4d6ebc03206c608e23f985e733c39ea
Zi Jian Sim <zijian12345@hotmail.com>
2018-11-23 23:07:37 +0000

    Added animation for switch

---
+++ Assets/ceilingLightToggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ceilingLightToggle : MonoBehaviour {
+
+    public GameObject ceilingLamp;
+    Light ceilingLight;
+    MeshRenderer meshRenderer;
+    public Material[] materials;
+	// Use this for initialization
+	void Start () {
+        ceilingLight = ceilingLamp.GetComponent<Light>();
+        meshRenderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void turnOn()
+    {
+        meshRenderer.material = materials[0];
+        ceilingLight.enabled = true;
+    }
+
+    public void turnOff()
+    {
+        meshRenderer.material = materials[1];
+        ceilingLight.enabled = false;
+    }
+}
---
+++ Assets/switchToggle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchToggle : MonoBehaviour {
+
+    bool switchOn = false;
+    public ceilingLightToggle ceilingLightToggle;
+    
+    GameObject switchPivot;
+    Light ceilingLight;
+	// Use this for initialization
+	void Start () {
+        switchPivot = this.gameObject.transform.GetChild(0).gameObject;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out raycastHit, Mathf.Infinity))
+            {
+                if (raycastHit.transform.name == "Switch")
+                {
+                    if (switchOn)
+                    {
+                        switchTurnOff();
+                    }
+                    else
+                    {
+                        switchTurnOn();
+                    }
+                }
+            }
+        }
+    }
+
+    void switchTurnOff()
+    {
+        //Do something with light
+        switchOn = false;
+        //Debug.Log("Off!");
+        switchPivot.transform.Rotate(20, 0, 0);
+        ceilingLightToggle.turnOff();
+    }
+
+    void switchTurnOn()
+    {
+        //Do something else with light
+        switchOn = true;
+        Debug.Log("On!");
+        switchPivot.transform.Rotate(-20, 0, 0);
+        ceilingLightToggle.turnOn();
+    }
+}
