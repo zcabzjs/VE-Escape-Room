a7be26bf9b9e3b988e14741a8b5d2a74fbd6e620
Sam Pham <sam.h.pham@gmail.com>
2018-11-23 01:00:01 +0000

    Interaction

--- Assets/BedroomDoorAnimation.cs
+++ Assets/BedroomDoorAnimation.cs
@@ -7,36 +7,25 @@ public class BedroomDoorAnimation : MonoBehaviour {
     bool bedroomDoorOpen;
     Animator bedroomDoorAnim;
 
-	// Use this for initialization
-	void Start () {
-        bedroomDoorAnim = GetComponent<Animator>();
-	}
-
-    // Update is called once per frame
-    void Update()
+    // Use this for initialization
+    void Start()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit raycastHit;
+        bedroomDoorAnim = GetComponent<Animator>();
+    }
 
-            if (Physics.Raycast(ray.origin, ray.direction, out raycastHit, Mathf.Infinity))
-            {
-                if (raycastHit.transform.name == "BedroomDoor")
-                {
-                    if (bedroomDoorOpen)
-                    {
-                        bedroomDoorAnim.SetBool("openDoor", false);
-                        bedroomDoorOpen = false;
-                    }
-                    else
-                    {
-                        bedroomDoorAnim.SetBool("openDoor", true);
-                        bedroomDoorOpen = true;
-                    }
-                   
-                }
-            }
+
+    void Trigger()
+    {
+        print("Hello!");
+        if (bedroomDoorOpen)
+        {
+            bedroomDoorAnim.SetBool("openDoor", false);
+            bedroomDoorOpen = false;
+        }
+        else
+        {
+            bedroomDoorAnim.SetBool("openDoor", true);
+            bedroomDoorOpen = true;
         }
     }
 }
