2262cdc03f4223a3404eedd4c6e95f436f09caf3
Zi Jian Sim <zijian12345@hotmail.com>
2018-11-26 21:58:55 +0000

    Added locked door prefab, tutorial switch and edited corridor to fit in utility room

---
+++ Assets/LockedDoorAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoorAnimation : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void Trigger()
+    {
+        anim.SetTrigger("openLockedDoor");
+    }
+
+}
---
+++ Assets/TutorialSwitchToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSwitchToggle : MonoBehaviour {
+
+    bool turnedOn = false;
+    GameObject switchPivot;
+    // Use this for initialization
+    void Start () {
+        switchPivot = this.gameObject.transform.GetChild(0).gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out raycastHit, Mathf.Infinity))
+            {
+                if (raycastHit.transform.name == "Wall switch")
+                {
+                    if (!turnedOn)
+                    {
+                        Debug.Log("Tutorial started!");
+                        switchPivot.transform.Rotate(10, 0, 0);
+                        turnedOn = true;
+                    }
+                }
+            }
+        }
+
+    }
+}
