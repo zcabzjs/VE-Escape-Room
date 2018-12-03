356b86714b7b684b104b493ec57f1758f3d97f51
Sam Pham <sam.h.pham@gmail.com>
2018-12-03 18:27:23 +0000

    Added Utilities room and ppuzzles

---
+++ Assets/utiliseRoom scene/keypad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playsound : MonoBehaviour
+
+{
+    public void Clicky()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+
+}
