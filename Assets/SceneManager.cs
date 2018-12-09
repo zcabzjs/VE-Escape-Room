e0d983353362731ae453425893bb188286a01545
Zi Jian Sim <zijian12345@hotmail.com>
2018-12-09 23:39:04 +0000

    Trying to add a customise full black skybox

--- Assets/SceneManager.cs
+++ Assets/SceneManager.cs
@@ -10,11 +10,12 @@ public class SceneManager : MonoBehaviour {
     public string[] levelNames = new string[2]{"StartScene", "Scene3"};
 
     static SceneManager s = null;
+
 	// Use this for initialization
 	void Start () {
 		if(s == null)
         {
-            s = this;
+            s = this;      
         }
         else
         {
@@ -25,11 +26,11 @@ public class SceneManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //if (Input.anyKeyDown)
-        //{
-        //    currLevel = (currLevel + 1) % 2;
-        //    SteamVR_LoadLevel.Begin(levelNames[currLevel]);
-        //}
+        if (Input.anyKeyDown)
+        {
+            currLevel = (currLevel + 1) % 2;
+            SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+        }
 	}
 
     public static void LoadPlayLevel()
