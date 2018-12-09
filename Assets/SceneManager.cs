4f1758743ec34367cd60e9afc312907df559bdb5
Zi Jian Sim <zijian12345@hotmail.com>
2018-12-09 21:57:17 +0000

    Added start scene for game

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SceneManager : MonoBehaviour {
+
+    public int currLevel = 1;
+
+    public string[] levelNames = new string[2]{"StartScene", "Scene3"};
+
+    static SceneManager s = null;
+	// Use this for initialization
+	void Start () {
+		if(s == null)
+        {
+            s = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            currLevel = (currLevel + 1) % 2;
+            SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+        }
+	}
+
+    public static void LoadPlayLevel()
+    {
+        SteamVR_LoadLevel.Begin("Scene3");
+    }
+}
