5006de1ca71e277b0215c013f5595e8de1c2a81a
Zi Jian Sim <zijian12345@hotmail.com>
2018-12-10 02:34:45 +0000

    Added end game interactions

---
+++ Assets/EndGameScreenInteraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameScreenInteraction : MonoBehaviour {
+
+    public Material[] materials;
+    MeshRenderer meshRenderer;
+    bool laptopTriggered;
+    // Use this for initialization
+    void Start () {
+        meshRenderer = GameObject.Find("Screen").GetComponent<MeshRenderer>();
+        StartCoroutine(ChangeFromCreditsToTheEnd());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator ChangeFromCreditsToTheEnd()
+    {
+        for (float timer = 5; timer >= 0; timer -= Time.deltaTime)
+        {
+            if (laptopTriggered)
+            {
+                switchToPlotTwistEnd();
+                yield break;
+            }
+            yield return null;
+        }
+        switchtoTheEndScreen();
+    }
+
+    void switchtoTheEndScreen()
+    {
+        meshRenderer.material = materials[0];
+    }
+
+    void switchToPlotTwistEnd()
+    {
+        meshRenderer.material = materials[1];      
+    }
+
+    public void triggerLaptop()
+    {
+        laptopTriggered = true;
+        switchToPlotTwistEnd();
+    }
+}
---
+++ Assets/LaptopEndInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaptopEndInteraction : MonoBehaviour {
+
+    public Material[] materials;
+    MeshRenderer meshRenderer;
+
+    public EndGameScreenInteraction endGame;
+
+    // Use this for initialization
+    void Start () {
+        meshRenderer = GameObject.Find("Laptop Screen").GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            turnOnEndLaptop();
+            endGame.triggerLaptop();
+        }
+
+    }
+
+    void Trigger()
+    {
+        turnOnEndLaptop();
+        endGame.triggerLaptop();
+    }
+
+    void turnOnEndLaptop()
+    {
+        Material[] currentMaterials;
+        currentMaterials = meshRenderer.materials;
+        currentMaterials[2] = materials[0];
+        meshRenderer.materials = currentMaterials;
+    }
+}
