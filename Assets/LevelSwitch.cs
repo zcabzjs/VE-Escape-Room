4120d4dea1b6b28eb95b50451098e07dc2cc47d1
Zi Jian Sim <zijian12345@hotmail.com>
2018-12-10 01:28:30 +0000

    Changed VR transitions method

--- Assets/LevelSwitch.cs
+++ Assets/LevelSwitch.cs
@@ -1,46 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
-public class SceneManager : MonoBehaviour {
+public class LevelSwitch : MonoBehaviour {
 
     public int currLevel = 1;
 
-    public string[] levelNames = new string[2]{"StartScene", "Scene3"};
+    public string[] levelNames = new string[2] { "StartScene", "Scene3" };
 
-    static SceneManager s = null;
+    static LevelSwitch s = null;
 
     private static float _fadeDuration = 2;
-	// Use this for initialization
-	void Start () {
-		if(s == null)
+    // Use this for initialization
+    void Start()
+    {
+        if (s == null)
         {
-            s = this;      
+            s = this;
         }
         else
         {
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this.gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //if (Input.anyKeyDown)
         //{
         //    currLevel = (currLevel + 1) % 2;
         //    SteamVR_LoadLevel.Begin(levelNames[currLevel]);
         //}
-	}
+    }
 
     public static void LoadPlayLevel()
     {
-
-        //First uses Steam VR transition, 2nd uses Unity transition
-
-        //SteamVR_LoadLevel.Begin("Scene3");
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene3");
+        //SteamVR_Fade.Start(Color.black, _fadeDuration);
+        SteamVR_LoadLevel.Begin("Scene3");
+        //SceneManager.LoadScene("Scene3");
     }
 }
